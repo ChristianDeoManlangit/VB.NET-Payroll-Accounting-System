d0041634ac02585f5dd64447f34f7df6e462a37e
Christian Deo Manlangit <95056955+ChristianDeoManlangit@users.noreply.github.com>
2025-04-26 15:20:39 +0800

    Add files via upload

---
+++ Payroll Accounting System/Form1.Designer.vb
@@ -0,0 +1,453 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MainLabel = New Label()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        TextBox11 = New TextBox()
+        TextBox12 = New TextBox()
+        TextBox13 = New TextBox()
+        TextBox14 = New TextBox()
+        TextBox15 = New TextBox()
+        TextBox16 = New TextBox()
+        TextBox17 = New TextBox()
+        TextBox18 = New TextBox()
+        TextBox19 = New TextBox()
+        TextBox20 = New TextBox()
+        TextBox21 = New TextBox()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' MainLabel
+        ' 
+        MainLabel.Anchor = AnchorStyles.None
+        MainLabel.AutoSize = True
+        MainLabel.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        MainLabel.Location = New Point(455, 44)
+        MainLabel.Name = "MainLabel"
+        MainLabel.Size = New Size(292, 36)
+        MainLabel.TabIndex = 0
+        MainLabel.Text = "Payroll Accounting System"
+        MainLabel.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Anchor = AnchorStyles.None
+        PictureBox1.Image = My.Resources.Resources._7
+        PictureBox1.Location = New Point(44, 23)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(165, 128)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.Anchor = AnchorStyles.None
+        Label1.AutoSize = True
+        Label1.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(45, 169)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(189, 36)
+        Label1.TabIndex = 0
+        Label1.Text = "Employee Name:"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.None
+        Label2.AutoSize = True
+        Label2.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(10, 204)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(29, 36)
+        Label2.TabIndex = 0
+        Label2.Text = "1."
+        Label2.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label3
+        ' 
+        Label3.Anchor = AnchorStyles.None
+        Label3.AutoSize = True
+        Label3.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(455, 169)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(68, 36)
+        Label3.TabIndex = 0
+        Label3.Text = "Rate:"
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label4
+        ' 
+        Label4.Anchor = AnchorStyles.None
+        Label4.AutoSize = True
+        Label4.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(574, 169)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(89, 36)
+        Label4.TabIndex = 0
+        Label4.Text = "Salary:"
+        Label4.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label5
+        ' 
+        Label5.Anchor = AnchorStyles.None
+        Label5.AutoSize = True
+        Label5.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(736, 169)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(74, 36)
+        Label5.TabIndex = 0
+        Label5.Text = "Total:"
+        Label5.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Anchor = AnchorStyles.None
+        TextBox1.Location = New Point(45, 208)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(265, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Anchor = AnchorStyles.None
+        TextBox2.Location = New Point(45, 258)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(265, 27)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Anchor = AnchorStyles.None
+        TextBox3.Location = New Point(45, 308)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(265, 27)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Anchor = AnchorStyles.None
+        TextBox4.Location = New Point(45, 358)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(265, 27)
+        TextBox4.TabIndex = 2
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Anchor = AnchorStyles.None
+        TextBox5.Location = New Point(332, 208)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(96, 27)
+        TextBox5.TabIndex = 2
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Anchor = AnchorStyles.None
+        TextBox6.Location = New Point(332, 258)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(96, 27)
+        TextBox6.TabIndex = 2
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Anchor = AnchorStyles.None
+        TextBox7.Location = New Point(332, 308)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(96, 27)
+        TextBox7.TabIndex = 2
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Anchor = AnchorStyles.None
+        TextBox8.Location = New Point(332, 358)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(96, 27)
+        TextBox8.TabIndex = 2
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Anchor = AnchorStyles.None
+        TextBox9.Location = New Point(455, 208)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(92, 27)
+        TextBox9.TabIndex = 2
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Anchor = AnchorStyles.None
+        TextBox10.Location = New Point(455, 258)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(92, 27)
+        TextBox10.TabIndex = 2
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Anchor = AnchorStyles.None
+        TextBox11.Location = New Point(455, 308)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(92, 27)
+        TextBox11.TabIndex = 2
+        ' 
+        ' TextBox12
+        ' 
+        TextBox12.Anchor = AnchorStyles.None
+        TextBox12.Location = New Point(455, 358)
+        TextBox12.Name = "TextBox12"
+        TextBox12.Size = New Size(92, 27)
+        TextBox12.TabIndex = 2
+        ' 
+        ' TextBox13
+        ' 
+        TextBox13.Anchor = AnchorStyles.None
+        TextBox13.Location = New Point(574, 208)
+        TextBox13.Name = "TextBox13"
+        TextBox13.Size = New Size(139, 27)
+        TextBox13.TabIndex = 2
+        ' 
+        ' TextBox14
+        ' 
+        TextBox14.Anchor = AnchorStyles.None
+        TextBox14.Location = New Point(574, 258)
+        TextBox14.Name = "TextBox14"
+        TextBox14.Size = New Size(139, 27)
+        TextBox14.TabIndex = 2
+        ' 
+        ' TextBox15
+        ' 
+        TextBox15.Anchor = AnchorStyles.None
+        TextBox15.Location = New Point(574, 308)
+        TextBox15.Name = "TextBox15"
+        TextBox15.Size = New Size(139, 27)
+        TextBox15.TabIndex = 2
+        ' 
+        ' TextBox16
+        ' 
+        TextBox16.Anchor = AnchorStyles.None
+        TextBox16.Location = New Point(574, 358)
+        TextBox16.Name = "TextBox16"
+        TextBox16.Size = New Size(139, 27)
+        TextBox16.TabIndex = 2
+        ' 
+        ' TextBox17
+        ' 
+        TextBox17.Anchor = AnchorStyles.None
+        TextBox17.Location = New Point(736, 208)
+        TextBox17.Name = "TextBox17"
+        TextBox17.Size = New Size(177, 27)
+        TextBox17.TabIndex = 2
+        ' 
+        ' TextBox18
+        ' 
+        TextBox18.Anchor = AnchorStyles.None
+        TextBox18.Location = New Point(736, 258)
+        TextBox18.Name = "TextBox18"
+        TextBox18.Size = New Size(177, 27)
+        TextBox18.TabIndex = 2
+        ' 
+        ' TextBox19
+        ' 
+        TextBox19.Anchor = AnchorStyles.None
+        TextBox19.Location = New Point(736, 308)
+        TextBox19.Name = "TextBox19"
+        TextBox19.Size = New Size(177, 27)
+        TextBox19.TabIndex = 2
+        ' 
+        ' TextBox20
+        ' 
+        TextBox20.Anchor = AnchorStyles.None
+        TextBox20.Location = New Point(736, 358)
+        TextBox20.Name = "TextBox20"
+        TextBox20.Size = New Size(177, 27)
+        TextBox20.TabIndex = 2
+        ' 
+        ' TextBox21
+        ' 
+        TextBox21.Anchor = AnchorStyles.None
+        TextBox21.Location = New Point(736, 408)
+        TextBox21.Name = "TextBox21"
+        TextBox21.Size = New Size(177, 27)
+        TextBox21.TabIndex = 2
+        ' 
+        ' Label6
+        ' 
+        Label6.Anchor = AnchorStyles.None
+        Label6.AutoSize = True
+        Label6.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(602, 404)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(117, 36)
+        Label6.TabIndex = 0
+        Label6.Text = "Total Pay:"
+        Label6.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label7
+        ' 
+        Label7.Anchor = AnchorStyles.None
+        Label7.AutoSize = True
+        Label7.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(10, 254)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(32, 36)
+        Label7.TabIndex = 0
+        Label7.Text = "2."
+        Label7.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label8
+        ' 
+        Label8.Anchor = AnchorStyles.None
+        Label8.AutoSize = True
+        Label8.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(10, 304)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(33, 36)
+        Label8.TabIndex = 0
+        Label8.Text = "3."
+        Label8.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label9
+        ' 
+        Label9.Anchor = AnchorStyles.None
+        Label9.AutoSize = True
+        Label9.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label9.Location = New Point(10, 354)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(35, 36)
+        Label9.TabIndex = 0
+        Label9.Text = "4."
+        Label9.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label10
+        ' 
+        Label10.Anchor = AnchorStyles.None
+        Label10.AutoSize = True
+        Label10.Font = New Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label10.Location = New Point(332, 169)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(82, 36)
+        Label10.TabIndex = 0
+        Label10.Text = "Hours:"
+        Label10.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(942, 493)
+        Controls.Add(TextBox21)
+        Controls.Add(TextBox20)
+        Controls.Add(TextBox16)
+        Controls.Add(TextBox12)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox19)
+        Controls.Add(TextBox15)
+        Controls.Add(TextBox11)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox18)
+        Controls.Add(TextBox14)
+        Controls.Add(TextBox10)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox17)
+        Controls.Add(TextBox13)
+        Controls.Add(TextBox9)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox1)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label2)
+        Controls.Add(Label10)
+        Controls.Add(Label1)
+        Controls.Add(MainLabel)
+        FormBorderStyle = FormBorderStyle.SizableToolWindow
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MainLabel As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents TextBox16 As TextBox
+    Friend WithEvents TextBox17 As TextBox
+    Friend WithEvents TextBox18 As TextBox
+    Friend WithEvents TextBox19 As TextBox
+    Friend WithEvents TextBox20 As TextBox
+    Friend WithEvents TextBox21 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+
+End Class
---
+++ Payroll Accounting System/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged, TextBox3.TextChanged, TextBox4.TextChanged, TextBox6.TextChanged, TextBox7.TextChanged, TextBox8.TextChanged, TextBox10.TextChanged, TextBox11.TextChanged, TextBox12.TextChanged, TextBox14.TextChanged, TextBox15.TextChanged, TextBox16.TextChanged, TextBox18.TextChanged, TextBox19.TextChanged, TextBox20.TextChanged, TextBox21.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged, TextBox5.TextChanged, TextBox9.TextChanged, TextBox13.TextChanged, TextBox17.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click, Label10.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click, Label9.Click
+
+    End Sub
+End Class
